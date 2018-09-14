d0ade7d731ecb0e197ca5a8a026b8ce22adf6840
Chris Dermody <chrisdermody1@gmail.com>
2018-09-14 19:42:08 +0100

    updated search icon to be linked, properly styled and animated

--- scripts.js
+++ scripts.js
@@ -4,98 +4,98 @@ let items = document.getElementsByClassName('carousel-item');
 // loop over each item, add click listener
 for (let i = 0, length1 = items.length; i < length1; i++) {
 
-    items[i].addEventListener("click", function(el) {
+	items[i].addEventListener("click", function(el) {
 
-        // need to know if card clicked has any cards previous to it with "expanded" class
-        let expandedIndex;
-        let clickedIndex = i;
+		// need to know if card clicked has any cards previous to it with "expanded" class
+		let expandedIndex;
+		let clickedIndex = i;
 
-        for (let index = 0, length1 = items.length; index < length1; index++) {
-            if (items[index].classList.value.match(/selected/)) {
-                expandedIndex = index
-            }
-        }
+		for (let index = 0, length1 = items.length; index < length1; index++) {
+			if (items[index].classList.value.match(/selected/)) {
+				expandedIndex = index
+			}
+		}
 
-        // close all other items that may be open
-        removeClass(items, "selected");
+		// close all other items that may be open
+		removeClass(items, "selected");
 
-        toggleExpanded(items[i]);
+		toggleExpanded(items[i]);
 
-        scrollToCard(items[i], expandedIndex, clickedIndex);
+		scrollToCard(items[i], expandedIndex, clickedIndex);
 
-    })
+	})
 }
 
 function scrollToCard(card, expandedIndex, clickedIndex) {
 
-    // Centres card in screen
-    let adjustment = 0;
+	// Centres card in screen
+	let adjustment = 0;
 
-    if (clickedIndex > expandedIndex) {
-        adjustment = 400;
-    }
+	if (clickedIndex > expandedIndex) {
+		adjustment = 400;
+	}
 
-    let left = card.offsetLeft;
+	let left = card.offsetLeft;
 
-    let elementWidth = 700;
+	let elementWidth = 700;
 
-    let screenwidth = window.outerWidth;
+	let screenwidth = window.outerWidth;
 
-    let target = left - adjustment - (screenwidth - elementWidth) / 2;
+	let target = left - adjustment - (screenwidth - elementWidth) / 2;
 
-    document.getElementById("carousel").scrollTo({
-        left: target,
-        behavior: "smooth"
-    });
+	document.getElementById("carousel").scrollTo({
+		left: target,
+		behavior: "smooth"
+	});
 }
 
 function removeClass(els, className) {
 
-    for (let i = 0, length1 = els.length; i < length1; i++) {
-        els[i]
-        if (els[i].classList)
-            els[i].classList.remove(className);
-        else
-            els[i].className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-    }
+	for (let i = 0, length1 = els.length; i < length1; i++) {
+		els[i]
+		if (els[i].classList)
+			els[i].classList.remove(className);
+		else
+			els[i].className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+	}
 }
 
 function toggleExpanded(el) {
-    let className = "selected";
-    if (el.classList) {
-        el.classList.toggle(className);
-    } else {
-        var classes = el.className.split(' ');
-        var existingIndex = classes.indexOf(className);
+	let className = "selected";
+	if (el.classList) {
+		el.classList.toggle(className);
+	} else {
+		var classes = el.className.split(' ');
+		var existingIndex = classes.indexOf(className);
 
-        if (existingIndex >= 0)
-            classes.splice(existingIndex, 1);
-        else
-            classes.push(className);
+		if (existingIndex >= 0)
+			classes.splice(existingIndex, 1);
+		else
+			classes.push(className);
 
-        el.className = classes.join(' ');
-    }
+		el.className = classes.join(' ');
+	}
 }
 
 function onLoaded() {
-    // pick first card
+	// pick first card
 
-    let firstCard = document.getElementsByClassName("carousel-item")[0];
+	let firstCard = document.getElementsByClassName("carousel-item")[0];
 
-    toggleExpanded(firstCard);
+	toggleExpanded(firstCard);
 
-    setTimeout(function() {
-        scrollToCard(firstCard);
-    }, 250)
+	setTimeout(function() {
+		scrollToCard(firstCard);
+	}, 250)
 
 }
 
 function ready(fn) {
-    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
-        fn();
-    } else {
-        document.addEventListener('DOMContentLoaded', fn);
-    }
+	if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
 }
 
 ready(onLoaded);
